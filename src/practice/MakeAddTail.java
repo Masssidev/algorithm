72bd181d0c4e3dbfb315a3cb5cb94c6e5ae32136
jaehee <akwogml112@gmail.com>
2018-08-11 14:42:27 +0900

    feat: 링크드 리스트 addTail 메서드 반복문으로 구현하기

---
+++ src/practice/MakeAddTail.java
@@ -0,0 +1,38 @@
+/*
+ * addTail 메소드 구현
+ * 출력: 0 1 2 3 4 5 6 7 8 9 10
+ */
+package practice;
+
+public class MakeAddTail {
+	static class Node {
+		int value;
+		Node next;
+
+		public Node(int value, Node next) {
+			this.value = value;
+			this.next = next;
+		}
+
+		public void printAll() {
+			System.out.print(" " + value);
+			if (next != null)
+				next.printAll();
+		}
+
+		public void addTail(int value) {
+			Node tempNode = this;
+			while (tempNode.next != null)
+				tempNode = tempNode.next;
+			tempNode.next = new Node(value, null);
+		}
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(0, null);
+		for (int i = 1; i <= 10; ++i)
+			root.addTail(i);
+
+		root.printAll();
+	}
+}
