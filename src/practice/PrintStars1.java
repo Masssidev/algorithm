149f00e551ec5c9d207e08a16f4e856a4b4eaf75
jaehee <akwogml112@gmail.com>
2018-11-05 00:05:08 +0900

    별찍기1

---
+++ src/practice/PrintStars1.java
@@ -0,0 +1,24 @@
+/*
+ * 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+ */
+package practice;
+
+import java.util.Scanner;
+
+public class PrintStars1 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		try (Scanner scan = new Scanner(System.in)) {
+			num = scan.nextInt();
+		}
+
+		for (int i = 0; i < num; ++i) {
+			for (int j = 0; j <= i; ++j) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
