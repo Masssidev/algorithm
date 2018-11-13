75aaef4653a862fa72d77c02c69bca0ed7abd11c
jaehee <akwogml112@gmail.com>
2018-11-13 15:43:07 +0900

    feat: 시험 성적

---
+++ src/practice/Grade.java
@@ -0,0 +1,30 @@
+/*
+ * 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+ */
+package practice;
+
+import java.util.Scanner;
+
+public class Grade {
+
+	public static void main(String[] args) {
+		int num = 0;
+
+		try (Scanner scan = new Scanner(System.in)) {
+			num = scan.nextInt();
+		}
+
+		if (num >= 90 && num <= 100)
+			System.out.println("A");
+		else if (num >= 80 && num <= 89)
+			System.out.println("B");
+		else if (num >= 70 && num <= 79)
+			System.out.println("C");
+		else if (num >= 60 && num <= 69)
+			System.out.println("D");
+		else
+			System.out.println("F");
+
+	}
+
+}
