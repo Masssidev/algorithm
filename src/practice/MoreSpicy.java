07141cb3245f47ec9834637967f0606fafbf0767
jaehee <akwogml112@gmail.com>
2019-03-25 11:55:14 +0900

    feat: 더맵게

---
+++ src/practice/MoreSpicy.java
@@ -0,0 +1,43 @@
+/*
+ * 매운 것을 좋아하는 Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다. 
+ * 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
+ * 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+ * Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
+ * Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때, 
+ * 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
+ */
+package practice;
+
+import java.util.PriorityQueue;
+
+public class MoreSpicy {
+
+	public static void main(String[] args) {
+		MoreSpicy theMoreSpicy = new MoreSpicy();
+		int[] scoville = { 1, 2, 3, 9, 10, 12 };
+		int k = 7;
+		System.out.println(theMoreSpicy.solution(scoville, k));
+	}
+
+	public int solution(int[] scoville, int K) {
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        for(int i = 0; i < scoville.length; i++)
+            q.add(scoville[i]);
+
+        int count = 0;
+        while(q.size() > 1 && q.peek() < K){
+            int weakHot = q.poll();
+            int secondWeakHot = q.poll();
+
+            int mixHot = weakHot + (secondWeakHot * 2);
+            q.offer(mixHot);
+            count++;
+        }
+
+        if(q.size() <= 1 && q.peek() < K)
+            count = -1;
+
+        return count;
+	}
+}
